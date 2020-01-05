c914c0533493cdee86cc81f99233186b0ea02f53
Mgrdich <mgotm13@gmail.com>
2020-01-05 16:41:29 +0400

    authentication part one

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const {User} = require("../models/user");
+const {hash, compare} = require("bcryptjs");
 
 exports.getLogin = (req, res) => {
-
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
@@ -9,15 +9,36 @@ exports.getLogin = (req, res) => {
     });
 };
 
+exports.getSignUp = (req, res) => {
+    res.render('auth/signup', {
+        pageTitle: 'Sign up',
+        path: '/signup',
+        isAuth: req.session.isLoggedIn
+    });
+};
+
 exports.postLogin = (req, res) => {
-    User.findById('5e0cba048ac37ef54cfa7f57')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email})
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                console.log(err); //after the save is done 
-                res.redirect('/');
+            if (!user) {
+                res.redirect('/login')
+            }
+            compare(password, user.password)
+                .then(function (result) {
+                    if (result) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(function () {
+                            res.redirect('/'); //after the save is done
+                        });
+                    }
+                    res.redirect('/login');
+                }).catch(function (err) {
+
             });
+
         }).catch(function (err) {
         console.log(err);
     });
@@ -26,6 +47,36 @@ exports.postLogin = (req, res) => {
 exports.postLogout = (req, res) => {
     req.session.destroy(function (err) {
         res.redirect('/');
+        console.log(err);
+    });
+};
+
+exports.postSignUp = (req, res) => {
+    console.log("signup");
+    const email = req.body.email;
+    const password = req.body.password;
+    const confimPassword = req.body.confirmPassword;
+
+    User.findOne({email: email})
+        .then(function (userDoc) {
+            console.log(userDoc);
+            if (userDoc) {
+                return res.redirect('/signup'); //bcz of this return is not a promise
+            }
+            return hash(password, 12)
+                .then(function (hashedPassword) {
+
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: {items: []}
+                    });
+                    return user.save();
+
+                }).then(function () {
+                    res.redirect('/login');
+                })
+        }).catch(function (err) {
         console.log(err);
     });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,16 @@ const {Router} = require('express');
 
 const router = Router();
 
-const {getLogin, postLogin, postLogout} = require("../controllers/auth.js");
+const {getLogin, postLogin, postLogout, getSignUp, postSignUp} = require("../controllers/auth.js");
 
 router.get('/login', getLogin);
+
+router.get('/signup', getSignUp);
 
 router.post('/login', postLogin);
 
 router.post('/logout', postLogout);
+
+router.post('/signup', postSignUp);
 
 module.exports = router;
